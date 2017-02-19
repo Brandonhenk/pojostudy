1b08a2268f54a8fe7b655224f79a998a4ef4ea16
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-19 10:13:04 -0600

    Studying Pojo

---
+++ edu/jalc/pojostudy/PojoStudy.java
@@ -0,0 +1,42 @@
+package edu.jalc.pojostudy;
+
+class PojoStudy{
+
+  private Width width;
+  private Hight height;
+  private final String color; //just to practice accesors
+
+  private PojoStudy(){
+    this.width = null;
+    this.height = null;
+    this.color = "black";
+  }
+
+  public PojoStudy(Width width, Height height, String color){
+    this.setWidth(width);
+    this.setHeight(height);
+    this.color = color;
+  }
+
+  public Width getWidth(){
+    return this.width;
+  }
+  public Height getHeight(){
+    return this.height;
+  }
+  public String getColor(){
+    return this.color;
+  }
+
+  //Mutators
+  Width setWidth(width){
+    this.width = width;
+    return this.width;
+  }
+  Height setHeight(height){
+    this.height = height;
+    return this.height;
+  }
+
+
+}
