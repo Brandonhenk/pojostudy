a39454348998f0dfeab8d85ae460d2a63c2a9f48
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-19 10:29:33 -0600

    Fixing Width

---
+++ edu/jalc/rectangle/width/Width.java
@@ -0,0 +1,13 @@
+package edu.jalc.rectangle.width;
+
+public class Width{
+  private final double width;
+
+  public Width(double width){
+    this.width = width;
+  }
+
+  public double getWidth(){
+    return this.width;
+  }
+}
