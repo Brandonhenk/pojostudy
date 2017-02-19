49fa74bc88b4301df3873da377e2063dba5cb1ff
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-02-19 10:32:58 -0600

    Height

---
+++ edu/jalc/rectangle/height/Height.java
@@ -0,0 +1,13 @@
+package edu.jalc.rectangle.height;
+
+public class Height{
+  private final double height;
+
+  public Height(double height){
+    this.height = height;
+  }
+
+  public double getHeight(){
+    return this.height;
+  }
+}
